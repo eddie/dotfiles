795e361bc9789173fca5174f2c31e1efbca2aa5f
Eddie Blundell <eblundell@gmail.com>
2020-01-09 10:41:00 +0000

    Docker compose alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 abbr gs 'git status'
 abbr ggp 'git push'
 abbr ggl 'git pull'
+abbr gcm 'git checkout master'
+abbr dcl 'docker-compose logs --tail=1000 -f'
+abbr dc 'docker-compose'
 alias h="history | grep"
