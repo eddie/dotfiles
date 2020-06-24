88e994f214d16ea4f6a81c145cc3014192aaa1fa
Eddie Blundell <eblundell@gmail.com>
2020-06-24 08:10:21 +0100

    Add fishfile

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ abbr gcm 'git checkout master'
 abbr dcl 'docker-compose logs --tail=1000 -f'
 abbr dc 'docker-compose'
 alias h="history | grep"
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
