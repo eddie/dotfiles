a5f653d0ebda2904fbf8486970288c4f14a18b65
Eddie Blundell <eblundell@gmail.com>
2025-04-03 17:07:33 +0100

    Make it clear when inside ssh session

--- .bashrc.d/20-prompt.sh
+++ .bashrc.d/20-prompt.sh
@@ -38,6 +38,63 @@ function parse_git_branch(){
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
+# Determine if we're in an SSH session
+function is_ssh_session() {
+  if [[ -n "$SSH_CONNECTION" || -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
+    return 0  # True, this is an SSH session
+  else
+    return 1  # False, not an SSH session
+  fi
+}
+
+# Set the hostname display based on SSH status
+function set_hostname_display() {
+  if is_ssh_session; then
+    # In SSH session - use a different color
+    HOSTNAME_COLOR="${LIGHT_RED}"
+    HOSTNAME_PREFIX="[SSH] "
+  else
+    # Local session - use the default green
+    HOSTNAME_COLOR="${GREEN}"
+    HOSTNAME_PREFIX=""
+  fi
+}
+
+#!/bin/bash
+#
+# DESCRIPTION:
+#
+#   Set the bash prompt according to:
+#    * the active virtualenv
+#    * the branch of the current git/mercurial repository
+#    * the return value of the previous command
+#    * the fact you just came from Windows and are used to having newlines in
+#      your prompts.
+#
+# USAGE:
+#
+#   1. Save this file as ~/.bash_prompt
+#   2. Add the following line to the end of your ~/.bashrc or ~/.bash_profile:
+#        . ~/.bash_prompt
+#
+# LINEAGE:
+#
+#   Based on work by woods
+#
+#   https://gist.github.com/31967
+
+# The various escape codes that we can use to color our prompt.
+        RED="\[\033[0;31m\]"
+     YELLOW="\[\033[1;33m\]"
+      GREEN="\[\033[0;32m\]"
+       BLUE="\[\033[1;34m\]"
+     PURPLE="\[\033[0;35m\]"
+  LIGHT_RED="\[\033[1;31m\]"
+LIGHT_GREEN="\[\033[1;32m\]"
+      WHITE="\[\033[1;37m\]"
+ LIGHT_GRAY="\[\033[0;37m\]"
+ COLOR_NONE="\[\e[0m\]"
+
 
 
 # Determine the branch/state information for this git repository.
@@ -79,10 +136,13 @@ function set_bash_prompt () {
 
   # Set the BRANCH variable.
   set_git_branch
+  
+  # Set the hostname display based on SSH status
+  set_hostname_display
 
   # Set the bash prompt variable.
   PS1="
-${PYTHON_VIRTUALENV}${GREEN}\u@\h${COLOR_NONE}:${YELLOW}\w${COLOR_NONE}${BRANCH}
+${PYTHON_VIRTUALENV}${HOSTNAME_PREFIX}${GREEN}\u@${HOSTNAME_COLOR}\h${COLOR_NONE}:${YELLOW}\w${COLOR_NONE}${BRANCH}
 ${PROMPT_SYMBOL} "
 }
 
